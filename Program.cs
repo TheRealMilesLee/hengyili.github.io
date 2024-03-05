df5cda218f92ac01e22b09c70585e735b4c005f9
TheRealMilesLee <grandtheftauto710@outlook.com>
2024-03-05 12:03:19 -0800

    Update the animate

--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 using hengyili.github.io;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-
+using Blazorise;
+using Blazorise.Bootstrap;
+using Blazorise.Icons.FontAwesome;
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+builder.Services
+  .AddBlazorise()
+  .AddBootstrapProviders()
+  .AddFontAwesomeIcons();
 await builder.Build().RunAsync();
